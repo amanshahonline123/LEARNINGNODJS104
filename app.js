d72d1cb53eb09fac6fa52a5dd78da718e1e830b8
amanshahonline <aksonline12345678@gmail.com>
2025-11-19 20:22:22 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("welcome to node.js")
+const a = 10
+console.log(a)
+// main file for NodeJS
+
+const b = "aman";
+console.log(b, a+a)
+
+//array
+const arr = [1,2,3,4,5]
+console.log(arr[1], arr.length)
+
+//object
+const person = {
+    name:"aman",
+    age:28
+}
+console.log(person.name, person.age)
