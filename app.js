cf811ca3f553d9e33b165cff60761cff287fb08e
amanshahonline <aksonline12345678@gmail.com>
2025-11-20 22:34:17 +0545

    express, api

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-//Hello World
+const app = require("express")()
 
-console.log("welcome to node.js")
-const a = 10
-console.log(a)
-// main file for NodeJS
+//alternative of above is following 2 lines:-
+  //const express = require("express")
+  //const app = express()
 
-const b = "aman";
-console.log(b, a+a)
+app.get("/", (req, res)=>{
+    res.json({
+        message:"Aman is a good boy",
+    })
+})
 
-//array
-const arr = [1,2,3,4,5]
-console.log(arr[1], arr.length)
-
-//object
-const person = {
-    name:"aman",
-    age:28
-}
-console.log(person.name, person.age)
+app.listen(2000, (req, res)=>{
+    console.log("NodeJS project")
+})
