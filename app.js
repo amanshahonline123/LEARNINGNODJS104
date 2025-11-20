0d078b1ae0b3a61d8d2ccfe1b455a7c8441bb093
amanshahonline <aksonline12345678@gmail.com>
2025-11-21 00:07:21 +0545

    json, get api, postman

--- app.js
+++ app.js
@@ -7,9 +7,23 @@ const app = require("express")()
 app.get("/", (req, res)=>{
     res.json({
         message:"Aman is a good boy",
+        query:"how about you?"
     })
 })
 
-app.listen(2000, (req, res)=>{
-    console.log("NodeJS project")
+app.get("/about", (req, res)=>{
+    res.json({
+        status:200,
+        message:"we are in about page"
+    })
+})
+
+app.get("/contact", (req, res)=>{
+    res.json({
+        message:"we are in contacts page"
+    })
+})
+
+app.listen(3000, (req, res)=>{
+    console.log("NodeJS project is running on port 3000")
 })
